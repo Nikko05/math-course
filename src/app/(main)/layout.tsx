e9aac166319b6f55e99dbbdb72f8ec33bc3c5d0d
Nikko05 <nikodemkusiak59@gmail.com>
2026-06-20 11:55:07 +0200

    Update path

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import '@/styles/globals.css';
+import '@/styles/global.css';
 import NavigationBar from "@/components/NavigationBar";
 import Footer from "@/components/Footer";
 
